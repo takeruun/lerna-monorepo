b1c021e820d7f662af6bc7cd772e4f13d220d6c6
takeru.hashimoto <hstake25@gmail.com>
2023-02-09 00:36:57 +0900

    add test

--- packages/app1/src/index.spec.ts
+++ packages/app1/src/index.spec.ts
@@ -1,9 +1,15 @@
-import { exec } from './index';
+import { exec, execV } from './index';
 
 describe('index spec', () => {
   it('exec', () => {
     const res = exec();
 
     expect(res).toBe('PEN_JS');
+  });
+
+  it('execV', () => {
+    const res = execV();
+
+    expect(res).toBe('PEN_JS-V');
   });
 });
