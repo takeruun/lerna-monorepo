9401fe0849c99be0106da0fc727209d6af802109
takeru.hashimoto <hstake25@gmail.com>
2023-02-09 00:02:04 +0900

    lint

--- packages/app1/src/index.spec.ts
+++ packages/app1/src/index.spec.ts
@@ -1,9 +1,9 @@
 import { exec } from './index';
 
-describe('index spec',()=>{
-  it('exec',()=>{
+describe('index spec', () => {
+  it('exec', () => {
     const res = exec();
-    
-    expect(res).toBe('PEN_JS')
+
+    expect(res).toBe('PEN_JS');
   });
 });
